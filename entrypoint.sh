ceea34294d39331d91b04c70b2edcd85019ea391
Ray Milstrey <rmilstrey@campuslabs.com>
2019-09-30 14:19:16 -0400

    It's monday.

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
 fi
 
 REPLACEMENT="assets"
-SOURCE_DIR_FORMATTED=${SOURCE_DIR/refs\/heads/$REPLACEMENT}
+DESTINATION_DIR_FORMATTED=${DESTINATION_DIR/refs\/heads/$REPLACEMENT}
 
 # Upload source to storage
-az storage blob upload-batch -s ${SOURCE_DIR} -d ${DESTINATION_DIR} --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_STORAGE_ACCOUNT_KEY} --connection-string ${AZURE_CONNECTION_STRING} --subscription ${AZURE_SUBSCRIPTION_ID}
+az storage blob upload-batch -s ${SOURCE_DIR} -d ${DESTINATION_DIR_FORMATTED} --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_STORAGE_ACCOUNT_KEY} --connection-string ${AZURE_CONNECTION_STRING} --subscription ${AZURE_SUBSCRIPTION_ID}
