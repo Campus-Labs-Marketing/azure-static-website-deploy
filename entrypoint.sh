ae973f95e186e92e327e06ff9b779e92bbbade9c
Ray Milstrey <rmilstrey@campuslabs.com>
2019-09-30 13:30:40 -0400

    Updating the variable names.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,35 +2,35 @@
 
 set -e
 
-if [ -z "$SOURCE_DIR" ]; then
+if [ -z "$INPUT_SOURCE_DIR" ]; then
   echo "SOURCE_DIR is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$DESTINATION_DIR" ]; then
+if [ -z "$INPUT_DESTINATION_DIR" ]; then
   echo "DESTINATION_DIR is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$AZURE_STORAGE_ACCOUNT_NAME" ]; then
+if [ -z "$INPUT_AZURE_STORAGE_ACCOUNT_NAME" ]; then
   echo "AZURE_STORAGE_ACCOUNT_NAME is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$AZURE_STORAGE_ACCOUNT_KEY" ]; then
+if [ -z "$INPUT_AZURE_STORAGE_ACCOUNT_KEY" ]; then
   echo "AZURE_STORAGE_ACCOUNT_KEY is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$AZURE_CONNECTION_STRING" ]; then
+if [ -z "$INPUT_AZURE_CONNECTION_STRING" ]; then
   echo "AZURE_CONNECTION_STRING is not set. Quitting."
   exit 1
 fi
 
-if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
+if [ -z "$INPUT_AZURE_SUBSCRIPTION_ID" ]; then
   echo "AZURE_SUBSCRIPTION_ID is not set. Quitting."
   exit 1
 fi
 
 # Upload source to storage
-az storage blob upload-batch -s ${SOURCE_DIR} -d ${DESTINATION_DIR} --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_STORAGE_ACCOUNT_KEY} --connection-string ${AZURE_CONNECTION_STRING} --subscription ${AZURE_SUBSCRIPTION_ID}
+az storage blob upload-batch -s ${INPUT_SOURCE_DIR} -d ${INPUT_DESTINATION_DIR} --account-name ${INPUT_AZURE_STORAGE_ACCOUNT_NAME} --account-key ${INPUT_AZURE_STORAGE_ACCOUNT_KEY} --connection-string ${INPUT_AZURE_CONNECTION_STRING} --subscription ${INPUT_AZURE_SUBSCRIPTION_ID}
