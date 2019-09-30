0206fb7a8194cd4c65d900123d4d8f2d5a4d630b
Ray Milstrey <rmilstrey@campuslabs.com>
2019-09-30 14:10:20 -0400

    Adding string manipulation to this stage of the workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,5 +32,8 @@ if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
   exit 1
 fi
 
+REPLACEMENT="assets"
+SOURCE_DIR_FORMATTED=${SOURCE_DIR/refs\/heads/$REPLACEMENT)
+
 # Upload source to storage
 az storage blob upload-batch -s ${SOURCE_DIR} -d ${DESTINATION_DIR} --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_STORAGE_ACCOUNT_KEY} --connection-string ${AZURE_CONNECTION_STRING} --subscription ${AZURE_SUBSCRIPTION_ID}
